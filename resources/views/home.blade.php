12afb82cc0190fe51ca8b24b8bd3147b4dc88571
Rupon Sarkar <ruponsarkar108@gmail.com>
2026-05-30 10:45:06 +0530

    RJMS started

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,77 +20,52 @@
 
 
 
-        <div class="container-fluid pb-3">
+        <div class="container pb-3">
 
             <div class="row">
-                <div class="col-md-12">
-                    <section id="hero">
-                        <div id="heroCarousel" class="carousel slide carousel-fade" data-bs-ride="carousel">
 
-                            <!-- Indicators -->
-                            <ol id="hero-carousel-indicators" class="carousel-indicators"></ol>
 
-                            <div class="carousel-inner">
 
-                                <!-- Slide 1 -->
-                                <div class="carousel-item active"
-                                    style="background: url('{{ asset('assets/banner/j.jpg') }}') center center;">
-                                    <div class="carousel-container">
-                                        {{-- <div class="container text-center">
-                                            <h2>Welcome to <span>Your School Name</span></h2>
-                                            <p>Providing world-class education for every student.</p>
-                                            <a href="#about" class="btn-get-started scrollto">Learn More</a>
-                                        </div> --}}
+                <div class="col-md-3">
 
-                                        {{-- <div>
-                                            <h2>Welcome to <span>GSASR Publisher</span></h2>
-                                            <h3>A publisher of scholarly journals and other academic resources.</h3>
-                                        </div> --}}
+                    <div>
+                        <div class="mt-3">
+                            <a href="/manuscript" class="btn btn-primary w-100">Submit Manuscript</a>
+                        </div>
+                    </div>
+
+
+                    <section class="mb-3">
+                        @include('partials.top_editors')
+                    </section>
+
+                    <section class="mb-3">
+                        @include('partials.certificates')
+                    </section>
+
+
+                    <section class="mb-3">
+                        <div class="card-c">
+                            <div>
+                                <div class="card-header">
+                                    <div class="h-box">
+                                        <div class="h-box-text p-2">
+                                            Recent Articles
+                                        </div>
                                     </div>
                                 </div>
-
-                                <!-- Slide 2 -->
-                                <div class="carousel-item"
-                                    style="background: url('{{ asset('assets/banner/j2.jpg') }}') center center;">
-                                    <div class="carousel-container">
-                                        {{-- <div class="container text-center">
-                                            <h2>Smart Classrooms</h2>
-                                            <p>Interactive smart learning for a brighter future.</p>
-                                            <a href="#academics" class="btn-get-started scrollto">Academics</a>
-                                        </div> --}}
-
-                                        {{-- <div>
-                                            <h2>Submit you manuscript here</h2>
-                                            <div class="text-center">
-                                                <button class="btn btn-primary">Submit Your Manuscript</button>
-                                            </div>
-                                        </div> --}}
-                                    </div>
-                                </div>
-
-
-
-
                             </div>
-
-                            <!-- Navigation -->
-                            <a class="carousel-control-prev" href="#heroCarousel" role="button" data-bs-slide="prev">
-                                <span class="carousel-control-prev-icon bi bi-chevron-left"></span>
-                            </a>
-
-                            <a class="carousel-control-next" href="#heroCarousel" role="button" data-bs-slide="next">
-                                <span class="carousel-control-next-icon bi bi-chevron-right"></span>
-                            </a>
-
+                            @include('partials.articles')
                         </div>
                     </section>
+                    @include('partials.quicklinks1')
                 </div>
-            </div>
 
-            <div class="row">
+
+
                 <div class="col-md-9">
 
-                    <section class="py-2">
+                    {{-- <section class="py-2">
                         <div>
                             <div class="journal-slider swiper">
                                 <div class="swiper-wrapper align-items-center">
@@ -104,24 +79,9 @@
                                 </div>
                             </div>
                         </div>
-                    </section>
+                    </section> --}}
 
 
-
-                    <section class="py-2">
-                        <div class="card-c py-4">
-                            <div class="p-3 text-center">
-                                <h2 style="color: #1976d2; font-weight: bold;">Advancing Research Worldwide</h2>
-                                <div>
-                                    Submit your research, explore journals, and publish with confidence.
-                                </div>
-                                <div class="mt-3">
-                                    <a href="/manuscript" class="btn btn-primary">Submit Manuscript</a>
-                                    <a href="/journals" class="btn btn-danger">Explore Journals</a>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
                     <section class="py-2">
                         <div class="card-c py-4">
                             <div class="p-3 text-center">
@@ -426,9 +386,9 @@
 
 
 
-                    <section class="py-2">
+                    {{-- <section class="py-2">
                         @include('partials.indexings')
-                    </section>
+                    </section> --}}
 
                     <section class="py-2">
                         {{-- @include('partials.counts') --}}
@@ -447,32 +407,7 @@
 
                 {{-- right side   --}}
 
-                <div class="col-md-3">
-                    <section class="mb-3">
-                        @include('partials.top_editors')
-                    </section>
 
-                    <section class="mb-3">
-                        @include('partials.certificates')
-                    </section>
-
-
-                    <section class="mb-3">
-                        <div class="card-c">
-                            <div>
-                                <div class="card-header">
-                                    <div class="h-box">
-                                        <div class="h-box-text p-2">
-                                            Recent Articles
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            @include('partials.articles')
-                        </div>
-                    </section>
-                    @include('partials.quicklinks1')
-                </div>
             </div>
 
 
